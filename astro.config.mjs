706c5011a79565bcf9fd5c16b511f9d03b592bf4
kyr0 <info@aron-homberg.de>
2023-04-26 19:38:04 +0200

    chore: preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,22 @@
 import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
 import { defineConfig } from "astro/config";
 import config from "./src/config/config.json";
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 import { remarkReadingTime } from "./src/lib/remarkReadingTime";
-import lazyLoadPlugin from 'rehype-plugin-image-native-lazy-loading'
+import lazyLoadPlugin from "rehype-plugin-image-native-lazy-loading";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: cloudflare(),
   site: config.site.base_url,
   base: config.site.base_path,
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   integrations: [
-    react(),
+    preact({ compat: true }),
     sitemap(),
     tailwind({
       config: {
