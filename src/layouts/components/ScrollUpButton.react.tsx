d1ec877e6943862757e25b03b58e7d67a30a8521
Michael Mannseicher <m.mansi1@gmx.net>
2023-04-26 18:26:33 +0200

    add: scroll up button

---
+++ src/layouts/components/ScrollUpButton.react.tsx
@@ -0,0 +1,54 @@
+import theme from "@config/theme.json";
+import { useLayoutEffect, useState } from "react";
+import { BiArrowToTop } from "react-icons/bi/index.js";
+
+export const ScrollUpButton = () => {
+  const [visible, setVisible] = useState(false);
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+      /* you can also use 'auto' behaviour
+                         in place of 'smooth' */
+    });
+  };
+
+  useLayoutEffect(() => {
+    const toggleVisible = () => {
+      const scrolled = document.documentElement.scrollTop;
+      if (scrolled > 300) {
+        setVisible(true);
+      } else if (scrolled <= 300) {
+        setVisible(false);
+      }
+    };
+    toggleVisible();
+    window.addEventListener("scroll", toggleVisible);
+    return () => window.removeEventListener("scroll", toggleVisible);
+  }, []);
+
+  return (
+    <>
+      {visible && (
+        <div
+          onClick={scrollToTop}
+          style={{
+            position: "fixed",
+            bottom: 5,
+            right: 10,
+            width: 45,
+            height: 45,
+            cursor: "pointer",
+            borderRadius: 8,
+            border: `1px solid ${theme.colors.default.theme_color.primary}`,
+          }}
+        >
+          <BiArrowToTop
+            size={"lg"}
+            color={theme.colors.default.theme_color.primary}
+          />
+        </div>
+      )}
+    </>
+  );
+};
