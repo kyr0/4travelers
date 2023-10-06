39d4a2a987f0e3453950642e059c4296929ef640
Aron Homberg <info@aron-homberg.de>
2023-10-06 23:47:40 +0700

    email sending

--- api/contact.ts
+++ api/contact.ts
@@ -6,37 +6,25 @@ export const runtime = 'edge';
 
 export default async function handler(req, res) {
   const { body } = req;
-  /*
-    await Email.send({
-        to: 'hello@4travelers.blog',
-        from: 'me@example.com',
-        subject: 'Hello World',
-        html: '<h1>Hello World</h1>',
-    });
-    */
     
     if (req.method === 'POST') {
-      let email = body.email;
 
-      // Do whatever you need to do with the data here
-      // (e.g., store in a database, perform some logic, etc.)
+      console.log('test', {
+          to: config.contactinfo.email,
+          from: body.email,
+          subject: body.subject,
+          text: body.message + "\n\n by " + body.name,
+      })
 
-      // Respond to the client
-      res.status(200).send('Form submitted successfully!' + email + ':' + config.contactinfo.name);
+      await Email.send({
+          to: config.contactinfo.email,
+          from: body.email,
+          subject: body.subject,
+          text: body.message + "\n\n by " + body.name,
+      });
+
+      Response.redirect(`${config.site.base_url}${config.site.base_path}/contact-thank-you`, 301)
     } else {
-        // Handle any other HTTP methods
         res.status(405).send('Method Not Allowed');
     }
-
-    // Response.redirect(`${config.site.base_url}${config.site.base_path}/contact-thank-you`, 301)
-
-    return new Response(JSON.stringify(body), {
-      status: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Max-Age': '86400',
-      },
-    });
 }
