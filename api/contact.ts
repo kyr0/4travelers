177b9ea55bd0091c84b8f5b86d3c67dee619e8c9
Aron Homberg <info@aron-homberg.de>
2023-10-06 23:55:47 +0700

    fix

--- api/contact.ts
+++ api/contact.ts
@@ -1,29 +1,24 @@
 import Email from 'vercel-email';
-import config from "../src/config/config.json";
-export const runtime = 'edge';
+import emailConfig from "../src/config/config.json";
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+ 
+export const config = {
+  runtime: 'edge', 
+};
 
-// config.contactinfo.name
-
-export default async function handler(req, res) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { body } = req;
     
     if (req.method === 'POST') {
 
-      console.log('test', {
-          to: config.contactinfo.email,
-          from: body.email,
-          subject: body.subject,
-          text: body.message + "\n\n by " + body.name,
-      })
-
       await Email.send({
-          to: config.contactinfo.email,
+          to: emailConfig.contactinfo.email,
           from: body.email,
           subject: body.subject,
           text: body.message + "\n\n by " + body.name,
       });
 
-      Response.redirect(`${config.site.base_url}${config.site.base_path}/contact-thank-you`, 301)
+      Response.redirect(`${emailConfig.site.base_url}${emailConfig.site.base_path}/contact-thank-you`, 301)
     } else {
         res.status(405).send('Method Not Allowed');
     }
